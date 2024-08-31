eef3c82fdef15fc21fb52e687f4a5601625883bc
calilisantos <calilisantos@gmail.com>
2024-08-31 00:54:20 -0300

    feat(spark_chart): add files

---
+++ spark_chart/files/features.py
@@ -0,0 +1,66 @@
+from pyspark.sql import functions as F, SparkSession
+
+# start sparksession
+spark = (
+    SparkSession.builder
+        .appName("clean_code_release")
+            .master("local[*]")
+                .getOrCreate()
+)
+
+# this conf ref: https://sparkbyexamples.com/spark/spark-stop-info-and-debug-logging-console/
+spark.sparkContext.setLogLevel("ERROR")
+
+# create sample data
+print("[INFO] Sample data\n")
+
+data = [
+    {
+        "client.id": "1",
+        "client.name": "Mary"
+    },
+    {
+        "client.id": "2",
+        "client.name": "Anna"
+    }
+]
+
+dataframe = spark.createDataFrame(data)
+dataframe.show()
+
+# withColumns feature
+print("[INFO] Add multiple columns: withColumns (new in release 3.3.0)\n")
+
+new_columns = {
+    "client.country": F.lit("USA"),
+    "client.job": F.lit("Data Engineer")
+}
+
+print("[INFO] new_columns dict:\n", new_columns, "\n")
+
+dataframe = dataframe.withColumns(new_columns)
+# dataframe = dataframe.withColumn("client.country", F.lit("USA")) \
+#     .withColumn("client.job", F.lit("Data Engineer"))
+dataframe.show()
+
+# withColumnsRenamed feature
+print("[INFO] Rename multiple columns: withColumnsRenamed (new in release 3.4.0)\n")
+
+columns_renamed = {column: column.replace(".", "_") for column in dataframe.columns}
+
+print("[INFO] columns_renamed dict\n", columns_renamed, "\n")
+
+dataframe = dataframe.withColumnsRenamed(columns_renamed)
+# dataframe = (
+#     dataframe
+#         .withColumnRenamed("client.id", "client_id")
+#         .withColumnRenamed("client.name", "client_name")
+#         .withColumnRenamed("client.country", "client_country")
+#         .withColumnRenamed("client.job", "client_job")
+# )
+dataframe.show()
+
+# isEmpty feature
+print("[INFO] Empty dataframe check: dataframe.isEmpty() (new in release 3.3.0)\n")
+print("dataframe is empty?\n", dataframe.isEmpty())
+# print("dataframe is empty?\n", dataframe.count() > 0)
